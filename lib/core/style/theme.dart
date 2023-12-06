fdbb169bfe0ab30b7e2c6560a0399d5071a77e79
shabir pjr <halemwagih6@gmail.com>
2023-12-06 19:58:50 +0200

    +layout screen

--- lib/core/style/theme.dart
+++ lib/core/style/theme.dart
@@ -34,6 +34,12 @@ class ThemeApp{
           fontWeight: FontWeight.bold,
           color: Colors.white
         ),
+        displayLarge: TextStyle(
+            fontSize: 30,
+            fontFamily: "Ubuntu",
+            fontWeight: FontWeight.w800,
+            color: Colors.white
+        ),
         labelSmall: TextStyle(
             fontFamily: "Poppins",
             fontSize: 15,
